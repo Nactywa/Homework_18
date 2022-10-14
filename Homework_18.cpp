191080bcd2916481e3420f04201e516b59a32e93
Nastya Zbukareva <nactywa@mail.ru>
2022-10-14 12:03:57 +0300

    Добавьте файлы проекта.

---
+++ Homework_18.cpp
@@ -0,0 +1,46 @@
+﻿// Homework_18.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+
+
+void FullArray(int* const arr, const int size)
+{
+    for (int a = 0; a < size; a++)
+    {
+        arr[a] = rand() % 10;
+    }
+
+
+}
+
+
+void ShowArray(const int* const arr, const int size)
+{
+    for (int a = 0; a < size; a++)
+    {
+        cout << arr[a] << '\t';
+    }
+
+}
+
+
+void push_in(int*& arr, int& size, const int value)
+{
+
+}
+
+int main()
+{
+    int size = 6;
+    int* arr = new int[size];
+    FullArray(arr, size);
+    ShowArray(arr, size);
+
+
+
+
+    delete[] arr;
+}
